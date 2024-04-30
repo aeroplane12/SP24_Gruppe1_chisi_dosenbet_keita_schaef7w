478694a064af58424aa5fc392df23f06e869f60f
Schaef7w <Schaef7w@students.uni-marburg.de>
2024-04-30 10:03:15 +0200

    Basic Structure

--- src/main/java/model/Manager.java
+++ src/main/java/model/Manager.java
@@ -1,9 +1,32 @@
 package model;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Manager {
 
 
     public void csvReader(String path){
         //TODO CSV. Reader
+        Scanner scanner;
+        try{
+            scanner = new Scanner(new File(path), StandardCharsets.UTF_8);
+            String[] input = new String[15];
+            while (scanner.hasNext()){
+                input = scanner.nextLine().split("[,\n]");
+
+
+
+            }
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
