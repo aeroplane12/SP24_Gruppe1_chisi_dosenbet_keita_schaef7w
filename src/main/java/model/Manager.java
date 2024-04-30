3944790c542a662308291a0aa93a83996ed01fe0
Schaef7w <Schaef7w@students.uni-marburg.de>
2024-04-30 09:08:12 +0200

    Basic Structure

--- src/main/java/model/Manager.java
+++ src/main/java/model/Manager.java
@@ -1,4 +1,9 @@
 package model;
 
 public class Manager {
+
+
+    public void csvReader(String path){
+        //TODO CSV. Reader
+    }
 }
--- src/test/java/model/ManagerTest.java
+++ src/test/java/model/ManagerTest.java
@@ -1,7 +1,12 @@
 package model;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ManagerTest {
-
+    @Test
+    void csvReaderTest(){
+        //TODO Unit test
+    }
 }
