4f0355bdb4f92c3b60f00873e4d7cbe6be1f1a05
Tobias Chisi <tobiaschisi@gmail.com>
2024-05-01 14:40:35 +0200

    Cleaned up code a bit

--- src/main/java/model/Manager.java
+++ src/main/java/model/Manager.java
@@ -16,6 +16,7 @@ public class Manager {
         //default file with header
         csvReaderPeople(path, true);
     }
+
     public void csvReaderPeople(String path, boolean header) {
         Scanner scanner;
         //Temp shit
@@ -23,7 +24,7 @@ public class Manager {
         List<Couple> testCouple = new ArrayList<>();
         try {
             scanner = new Scanner(new File(path), StandardCharsets.UTF_8);
-            //skip first line
+            //skip the first line
             String[] input;
             if (scanner.hasNext() && header) {
                 scanner.nextLine();
@@ -44,19 +45,21 @@ public class Manager {
         }
 
     }
-    public void csvReaderPartyLocation(String path){
-        csvReaderPartyLocation(path,true);
+
+    public void csvReaderPartyLocation(String path) {
+        csvReaderPartyLocation(path, true);
     }
-    public void csvReaderPartyLocation(String path,boolean header){
+
+    public void csvReaderPartyLocation(String path, boolean header) {
         Scanner scanner;
-        try{
+        try {
             scanner = new Scanner(new File(path), StandardCharsets.UTF_8);
             String[] input;
             if (scanner.hasNext() && header) {
                 scanner.nextLine();
             }
             input = scanner.nextLine().split("[,\n]");
-            partyLoc = new Location(Double.parseDouble(input[0]),Double.parseDouble(input[1]));
+            partyLoc = new Location(Double.parseDouble(input[0]), Double.parseDouble(input[1]));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
