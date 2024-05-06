6b1dcd0b3bf3a3805d1cbe133399175deec2b2d3
Tobias Chisi <tobiaschisi@gmail.com>
2024-05-06 10:08:54 +0200

    Worked on Test cases

---
+++ src/test/java/model/GroupManagerTest.java
@@ -0,0 +1,31 @@
+package model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GroupManagerTest {
+    private GroupManager groupManager;
+
+    @BeforeEach
+    public void setUp() {
+        groupManager = new GroupManager();
+        // TODO: Initialize groupManager with appropriate data
+    }
+
+    @Test
+    public void testCalcGroups() {
+        // TODO: Implement this test method once calcGroups is implemented
+    }
+
+    @Test
+    public void testGenerateGroup() {
+        // TODO: Implement this test method once generateGroup is implemented
+    }
+
+    @Test
+    public void testPrintCluster() {
+        // TODO: Implement this test method once printCluster is implemented
+    }
+}
