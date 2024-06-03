de2e3058628624dacae1742ff8fc40d8c2c0c1a1
Dosenbet <dosenbet@uni-marburg.de>
2024-06-03 02:44:36 +0200

    Tried to implement a way to calculate the rating that calcCouples will be using

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
 import model.*;
+import model.tools.CSVReader;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,5 +10,8 @@ public class Main {
         Manager Manager = new Manager(groupManager, coupleManager);
         Manager.inputPeople("Dokumentation/TestingData/teilnehmerliste.csv");
         Manager.inputLocation("Dokumentation/TestingData/partylocation.csv");
+        List<Person> personList = CSVReader.csvReaderPeople("Dokumentation/TestingData/teilnehmerliste.csv");
+        List<Person> filteredList = personList.stream().filter(elem -> elem.getFoodPreference() == FoodPreference.FoodPref.NONE).toList();
+        System.out.println(personList.get(0).getGender().value == personList.get(5).getGender().value);
     }
 }
--- src/main/java/model/CoupleManager.java
+++ src/main/java/model/CoupleManager.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-
 public class CoupleManager {
     // might need overview over all People
     List<Person> allParticipants = new ArrayList<>();
@@ -21,6 +20,73 @@ public class CoupleManager {
         // - Sorted according to their Age Group
         // - no couple without a kitchen, no couple with the same kitchen
 
+        /*TODO
+            PFLICHT
+            - Esssensvorliebe :
+            Fleisch + Fleisch = Fleisch,
+            Fleisch + Egal = Fleisch,
+            Fleisch/Egal + Vegetarisch/Vegan = Vegetarisch/Vegan,
+            Vegan + Vegetarisch = Vegan,
+            Fleisch darf nicht mit Vegan/Vegetarisch gepaart werden (außer sie melden sich zusammen an)
+            - Die Küche wählen, welche am nächsten an der After-Dinner-Party ist, Pärchen ohne Küche ungültig
+            - Küchen dürfen nicht mehr als 3 mal als Paar besetzt werden
+            - Teilnehmer dürfen nur dann Pärchen bilden, wenn distanz zwischen ihnen nicht 0 ist
+            RANKING
+            - Möglichst identische Essensvorliebe
+            - Möglichst kleiner altersunterschied bei Pärchen
+            - Geschlechter sollten möglichst durchmischt sein
+            - Weglänge der Pärchen sollte möglichst klein sein, Laufrichtung sollte am besten in Richtung
+            Afterparty gehen. Man könnte die Quartile berechnen und somit die Locations ausmachen, welche sich
+            am besten für Vorspeise, Hauptspeise und Desert am besten eignen???
+            - Kann sein, dass Personen keinen Partnern zugeordnet werden können, weshalb diese mit Nachrückern zusammengebracht werden sollen
+        */
+        allSingleParticipants = allParticipants.stream().filter(person -> !person.hasPartner()).toList();
+        List<Person> fleshPrefGroup = allSingleParticipants.stream()
+                .filter(person -> person.getFoodPreference() == FoodPreference.FoodPref.MEAT ||
+                        person.getFoodPreference() == FoodPreference.FoodPref.NONE)
+                .toList();
+        List<Person> vegiGroup = allSingleParticipants.stream()
+                .filter(person -> !(person.getFoodPreference() == FoodPreference.FoodPref.MEAT))
+                .toList();
+
+        double[][] coupleRating = new double[allSingleParticipants.size()][allSingleParticipants.size()];
+        for (int i = 0; i < coupleRating.length; i++) {
+            for (int j = i + 1; j < coupleRating.length; j++) {
+
+            }
+        }
+    }
+    private double calculateCoupleRating(Person person1, Person person2){
+        // are the FoodPreferences compatible
+        if ((person1.getFoodPreference().value == 1 && person2.getFoodPreference().value > 1) ||
+            person2.getFoodPreference().value == 1 && person1.getFoodPreference().value > 1) {
+            return 0;
+        }
+        // does at least one person have a kitchen
+        if (person1.getKitchen() == null && person2.getKitchen() == null) {
+            return 0;
+        }
+        // are they living in the same building
+        if (person1.getKitchen().distance(person2.getKitchen()) == 0) {
+            return 0;
+        }
+        double rating = 1;
+        // if egali and vegi/vegan are tested, half the rating, prefer egali and meat
+        // 0.5 is a placeholder for now
+        if ((person1.getFoodPreference().value == 0 && person2.getFoodPreference().value > 1) ||
+            person2.getFoodPreference().value == 0 && person1.getFoodPreference().value > 1) {
+            rating = 0.5;
+        }
+        // computes a rating for the age difference,
+        // 0.1 is a placeholder for now
+        rating *= 1 - 0.1 * Math.abs(person1.getAge().value - person2.getAge().value) ;
+        // adjusting rating based on the gender of both ppl
+        // 0.7 another magic number
+        if (person1.getGender() == person2.getGender()) {
+            rating *= 0.7;
+        }
+
+        return rating;
 
     }
     public void addPerson(Person person){
