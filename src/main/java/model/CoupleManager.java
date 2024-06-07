05dcdbfdf37b59ad96beaa96e072b3298fdaaf14
Keita <keita@students.uni-marburg.de>
2024-06-07 15:00:55 +0200

    more test in CoupleManagerTest and change calculateCost in CoupleManager from private to public.

--- src/main/java/model/CoupleManager.java
+++ src/main/java/model/CoupleManager.java
@@ -246,7 +246,7 @@ class CoupleManager {
         }
     }
     //This is correct
-    private double calculateCost(Person person1, Person person2) {
+    public double calculateCost(Person person1, Person person2) {
         int cost = 10;
 
         if (person1.equals(person2))
