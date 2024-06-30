fc43d9edc2cb6c89080248062196df0fef7f84bf
Keita <keita@students.uni-marburg.de>
2024-06-30 21:21:20 +0200

    view now run.

--- src/main/java/sw/praktikum/spinfood/View.java
+++ src/main/java/sw/praktikum/spinfood/View.java
@@ -12,7 +12,7 @@ public class View extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(View.class.getResource("StartUpWindow.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Spinfood");
         stage.setScene(scene);
         stage.show();
     }
